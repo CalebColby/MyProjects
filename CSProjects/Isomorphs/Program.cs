be721839fea0164122e41f4e1398be4ebc0fc4f4
CalebColby <ccolby@student.neumont.edu>
2022-03-30 15:29:48 -0600

    Update Program.cs

--- CSProjects/Isomorphs/Program.cs
+++ CSProjects/Isomorphs/Program.cs
@@ -1,6 +1,17 @@
-﻿bool goodPath = false;
-
-do
+﻿
+String[] getIsoStringsFromFile()
 {
+    do 
+    {
+        Console.WriteLine("Give a file path:");
+        try
+        {
+            String filePath = Console.ReadLine();
+            return System.IO.File.ReadAllLines(filePath);
+        }  catch(FileNotFoundException fnfe) {
+            Console.WriteLine("Invalid File Path Given.\nCouldn't Find File.\nPlease Give a Valid File Path\n");
+        }
+    } while (true);
+}
 
-}while (!goodPath);
+
