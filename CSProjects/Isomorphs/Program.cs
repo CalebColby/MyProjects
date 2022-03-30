75df5abd3d24e87e4bce1081c8dca54b138630e4
CalebColby <ccolby@student.neumont.edu>
2022-03-30 16:06:15 -0600

    Update Program.cs

--- CSProjects/Isomorphs/Program.cs
+++ CSProjects/Isomorphs/Program.cs
@@ -1,20 +1,28 @@
-﻿
-string[] getIsoStringsFromFile()
+﻿class Program
 {
-    do 
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Give me a file path:");
-        try
-        {
-            string filePath = Console.ReadLine();
-            return System.IO.File.ReadAllLines(filePath);
-        }  catch(FileNotFoundException fnfe) {
-            Console.WriteLine("Invalid File Path Given.\nCouldn't Find File.\nPlease Give a Valid File Path\n");
-        }
-    } while (true);
-}
 
-void writeToFileAndConsole()
-{
-    String filepath = Path.;
+    }
+
+    string[] getIsoStringsFromFile()
+    {
+        do
+        {
+            Console.WriteLine("Give me a file path:");
+            try
+            {
+                string filePath = Console.ReadLine();
+                return System.IO.File.ReadAllLines(filePath);
+            } catch (FileNotFoundException fnfe) {
+                Console.WriteLine("Invalid File Path Given.\nCouldn't Find File.\nPlease Give a Valid File Path\n");
+            }
+        } while (true);
+    }
+
+    void writeToFileAndConsole()
+    {
+        String filepath = Path.;
+    }
+
 }
