53470319aacb6cb3eb825caa8c1423574289bc4a
CalebColby <ccolby@student.neumont.edu>
2022-03-30 18:42:35 -0600

    qwertyuiop

--- CSProjects/HelloWorld/Program.cs
+++ CSProjects/HelloWorld/Program.cs
@@ -4,9 +4,11 @@ namespace HelloWorld
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World");
+            Console.Write("What is your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
         }
     }
 }
