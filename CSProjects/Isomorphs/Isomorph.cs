6f15b9b2a3bc12148052b370e5c21143c00f7ea9
CalebColby <ccolby@student.neumont.edu>
2022-04-01 15:52:27 -0600

    Update Isomorph.cs

--- CSProjects/Isomorphs/Isomorph.cs
+++ CSProjects/Isomorphs/Isomorph.cs
@@ -15,30 +15,23 @@ namespace Isomorphs
         public string getIsoWord() { return this.isoWord; }
         public List<int> getIsoSign() { return this.isoSign; }
         public List<int> getLooseIsoSign() { return this.looseIsoSign; }
-
-        private static List<int> generateIsoSign(string word)
+        
+        public bool compareIsoSign(List<int> other)
         {
-            char[] wordArray = word.ToCharArray();
-            List<int> isoSign = new List<int>();
-            Dictionary<char, int> isoSignDictionary = new Dictionary<char, int>();
-            int dictTracker = 0;
-            foreach (char c in wordArray)
-            {
-                if(!isoSignDictionary.ContainsKey(c))
-                {
-                    isoSignDictionary.Add(c, dictTracker++);
-                }
-                isoSign.Add(isoSignDictionary[c]);
-            }
-
-            return isoSign;
+            if (other == null) return false;
+            if(isoSign.Count != other.Count) { return false; }
+            for (int i = 0; i < isoSign.Count; i++) 
+                    if (isoSign[i] != other[i]) return false; 
+            return true;
         }
 
-        private static List<int> generateLooseIsoSign(List<int> isoSign)
+        public bool compareLooseIsoSign(List<int> other)
         {
-            List<int> looseIsoSign = new List<int>();
-
-            return looseIsoSign;
+            if (other == null) return false;
+            if (looseIsoSign.Count != other.Count) { return false; }
+            for (int i = 0; i < looseIsoSign.Count; i++)
+                if (looseIsoSign[i] != other[i]) return false;
+            return true;
         }
 
         public void setIsoWord(string isoWord)
@@ -52,6 +45,40 @@ namespace Isomorphs
             this.setIsoWord(isoWord);
         }
 
-       
+
+
+        private static List<int> generateIsoSign(string word)
+        {
+            char[] wordArray = word.ToCharArray();
+            List<int> isoSign = new List<int>();
+            Dictionary<char, int> isoSignDictionary = new Dictionary<char, int>();
+            int dictTracker = 0;
+            foreach (char c in wordArray)
+            {
+                if (!isoSignDictionary.ContainsKey(c))
+                {
+                    isoSignDictionary.Add(c, dictTracker++);
+                }
+                isoSign.Add(isoSignDictionary[c]);
+            }
+
+            return isoSign;
+        }
+
+        private static List<int> generateLooseIsoSign(List<int> isoSign)
+        {
+            List<int> looseIsoSign = new List<int>();
+
+            foreach (int i in isoSign)
+            {
+                if (looseIsoSign.Count() <= i)
+                {
+                    looseIsoSign.Add(1);
+                }
+                looseIsoSign[i]++;
+            }
+
+            return looseIsoSign;
+        }
     }
 }
