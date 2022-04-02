e88fdbdfb13bcda570dc9304e05851735aaa3e8f
CalebColby <ccolby@student.neumont.edu>
2022-04-01 18:18:59 -0600

    s

---
+++ CSProjects/HelloWorld/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld
+{
+    public class Student
+    {
+        public string Name { get; private set; }
+        
+        public static void Greet(string name)
+        {
+            Student s = new Student();
+            Console.WriteLine($"Hello {name}");
+        }
+
+    }
+}
