57338f987cd7cd099e317368f04817f19f72586c
CalebColby <ccolby@student.neumont.org>
2022-06-30 16:19:12 -0600

    sdf

--- CSProjects/FiniteStateMachine/FiniteStateMachine/Program.cs
+++ CSProjects/FiniteStateMachine/FiniteStateMachine/Program.cs
@@ -4,7 +4,75 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool wasGreeted = false;
+            while (true)
+            {
+                int response = new Random().Next(3);
+                string input = Console.ReadLine();
+                if (isHigh(input))
+                {
+                    RespondToHi(response);
+                    wasGreeted = true;
+                }
+                else
+                {
+                    if (wasGreeted)
+                    {
+                        Respond(response);
+                    }
+                    else
+                    {
+                        RespondToNotGreeted(response);
+                    }
+                }
+            }
+        }
+
+        private static bool isHigh(string input)
+        {
+            char state = 'A';
+
+            foreach (char c in input)
+            {
+                switch (state)
+                {
+                    case 'Z':
+                        state = (c == ' ') ? 'A' : 'Z';
+                        break;
+                    case 'A':
+                        state = (c == 'h' || c == 'H') ? 'B' : 'Z';
+                        if (c == ' ') state = 'A';
+                        break;
+                    case 'B':
+                        state = (c == 'i' || c == 'I') ? 'C' : 'Z';
+                        if (c == ' ') state = 'A';
+                        break;
+                    case 'C':
+                        state = (c == ' ') ? 'D' : 'Z';
+                        break;
+                    case 'D':
+                        return true;
+                        break;
+                    default:
+                        throw new Exception($"Unexpected State: {state}");
+                }
+            }
+            return state == 'C' || state == 'D';
+        }
+
+        private static void RespondToHi(int res)
+        {
+            Console.WriteLine("You said hi");
+        }
+
+        private static void Respond(int res)
+        {
+            Console.WriteLine("You didn't say hi");
+        }
+
+        private static void RespondToNotGreeted(int res)
+        {
+            Console.WriteLine("You haven't greeted me");
         }
     }
 }
