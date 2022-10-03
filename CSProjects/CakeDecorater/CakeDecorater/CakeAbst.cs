0c80369aee0fc9051e41de8e383f3aafbdd1457b
CalebColby <ccolby@student.neumont.org>
2022-10-03 15:08:22 -0600

    asdf

---
+++ CSProjects/CakeDecorater/CakeDecorater/CakeAbst.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CakeDecorater
+{
+    public abstract class CakeAbst
+    {
+        protected ICakeItem SubItem = null;
+
+
+        //This is my ctor to link other options
+        public CakeAbst(ICakeItem subItem)
+        {
+            this.SubItem = subItem;
+        }
+    }
+}
---
+++ CSProjects/CakeDecorater/CakeDecorater/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CakeDecorater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
