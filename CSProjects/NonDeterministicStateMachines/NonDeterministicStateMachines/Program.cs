1b59c65e7912aeb1b612200a91cb8a0275e665c6
CalebColby <ccolby@student.neumont.edu>
2023-07-12 21:39:37 -0600

    July12Commit

--- CSProjects/NonDeterministicStateMachines/NonDeterministicStateMachines/Program.cs
+++ CSProjects/NonDeterministicStateMachines/NonDeterministicStateMachines/Program.cs
@@ -18,9 +18,7 @@ namespace NonDeterministicStateMachines
             Console.WriteLine("Welcome to the Non-Deterministic Finite State Machine");
             while(true)
             {
-                Console.WriteLine("--------------------------------------------------------------");
-                Console.WriteLine("Please Enter Something");
-                Console.WriteLine("--------------------------------------------------------------");
+                Console.WriteLine("--------------------------------------------------------------\nPlease Enter Something");
                 string input = Console.ReadLine();
                 if (!string.IsNullOrEmpty(input))
                 {
@@ -40,87 +38,261 @@ namespace NonDeterministicStateMachines
         private static bool CheckHigh(string input)
         {
             char state = 'A';
-            foreach (char c in input)
+            foreach (char let in input)
             {
                 switch(state)
                 {
                     case 'A':
+                        if(let == 'H' || let == 'h')  state = 'B'; 
+                        else state = 'Z'; 
+                        break;
+                    case 'B':
+                        if(let == 'i') state = 'C'; 
+                        else state = 'Z'; 
+                        break;
+                    case 'C':
+                        if(let == ' ')
+                        {
+                            state = 'D';
+                            return true;
+                        }
+                        else state = 'Z'; 
+                        break;
+                    case 'D': // technically unreachable, but listed here for completeness anyway
+                        return true;
+                    case 'Z':
+                        if(let == ' ') state = 'A';
                         break;
                     default:
                         throw new ArgumentException($"Unknown State :{state}, Input was {input}");
                 }
+                if (state == 'Z' && let == ' ') state = 'A';
             }
-            throw new NotImplementedException();
+            return (state == 'C'); //EoS Checker
         }
 
         private static bool CheckHello(string input)
         {
             char state = 'A';
-            foreach (char c in input)
+            foreach (char let in input)
             {
                 switch (state)
                 {
                     case 'A':
+                        if (let == 'H' || let == 'h') state = 'B'; 
+                        else state = 'Z'; 
+                        break;
+                    case 'B':
+                        if(let == 'e') state = 'C'; 
+                        else state = 'Z';
+                        break;
+                    case 'C':
+                        if (let == 'l') state = 'D';
+                        else state = 'Z';
+                        break;
+                    case 'D':
+                        if (let == 'l') state = 'E';
+                        else state = 'Z';
+                        break;
+                    case 'E':
+                        if (let == 'o') state = 'F';
+                        else state = 'Z';
+                        break;
+                    case 'F':
+                        if (let == ' ')
+                        {
+                            state = 'G';
+                            return true;
+                        }
+                        else state = 'Z';
+                        break;
+                    case 'G': //technically unreachable, but here for completeness anyway
+                        return true;
+                    case 'Z':
+                        if(let == ' ') state = 'A';
                         break;
                     default:
                         throw new ArgumentException($"Unknown State :{state}, Input was {input}");
                 }
+                if (state == 'Z' && let == ' ') state = 'A';
             }
-            throw new NotImplementedException();
+            return (state == 'F'); //EoS Checker
         }
 
         private static bool CheckCowboy(string input)
         {
             char state = 'A';
-            foreach (char c in input)
+            foreach (char let in input)
             {
-                switch(state)
+                switch (state)
                 {
                     case 'A':
+                        if (let == 'H' || let == 'h') state = 'B';
+                        else state = 'Z';
+                        break;
+                    case 'B':
+                        if (let == 'o') state = 'C';
+                        else state = 'Z';
+                        break;
+                    case 'C':
+                        if (let == 'w') state = 'D';
+                        else state = 'Z';
+                        break;
+                    case 'D':
+                        if (let == 'd') state = 'E';
+                        else state = 'Z';
+                        break;
+                    case 'E':
+                        if (let == 'y') state = 'F';
+                        else state = 'Z';
+                        break;
+                    case 'F':
+                        if (let == ' ')
+                        {
+                            state = 'G';
+                            return true;
+                        }
+                        else state = 'Z';
+                        break;
+                    case 'G': //technically unreachable, but here for completeness anyway
+                        return true;
+                    case 'Z':
+                        if (let == ' ') state = 'A';
                         break;
                     default:
                         throw new ArgumentException($"Unknown State :{state}, Input was {input}");
                 }
+                if (state == 'Z' && let == ' ') state = 'A';
             }
-            throw new NotImplementedException();
+            return (state == 'F'); //EoS Checker
         }
 
         private static bool CheckAloha(string input)
         {
             char state = 'A';
-            foreach (char c in input)
+            foreach (char let in input)
             {
-                switch(state)
+                switch (state)
                 {
                     case 'A':
+                        if (let == 'A' || let == 'a') state = 'B';
+                        else state = 'Z';
+                        break;
+                    case 'B':
+                        if (let == 'l') state = 'C';
+                        else state = 'Z';
+                        break;
+                    case 'C':
+                        if (let == 'o') state = 'D';
+                        else state = 'Z';
+                        break;
+                    case 'D':
+                        if (let == 'h') state = 'E';
+                        else state = 'Z';
+                        break;
+                    case 'E':
+                        if (let == 'a') state = 'F';
+                        else state = 'Z';
+                        break;
+                    case 'F':
+                        if (let == ' ')
+                        {
+                            state = 'G';
+                            return true;
+                        }
+                        else state = 'Z';
+                        break;
+                    case 'G': //technically unreachable, but here for completeness anyway
+                        return true;
+                    case 'Z':
+                        if (let == ' ') state = 'A';
                         break;
                     default:
                         throw new ArgumentException($"Unknown State :{state}, Input was {input}");
                 }
+                if (state == 'Z' && let == ' ') state = 'A';
             }
-            throw new NotImplementedException();
+            return (state == 'F'); //EoS Checker
         }
 
         private static bool CheckGratitude(string input)
         {
             char state = 'A';
-            foreach (char c in input)
+            foreach (char let in input)
             {
                 switch(state)
                 {
                     case 'A':
+                        if (let == 'T' || let == 't') state = 'B';
+                        else state = 'Z';
+                        break;
+                    case 'B':
+                        if (let == 'h') state = 'C';
+                        else state = 'Z';
+                        break;
+                    case 'C':
+                        if (let == 'a') state = 'D';
+                        else state = 'Z';
+                        break;
+                    case 'D':
+                        if (let == 'n') state = 'E';
+                        else state = 'Z';
+                        break;
+                    case 'E':
+                        if (let == 'k') state = 'F';
+                        else state = 'Z';
+                        break;
+                    case 'F':
+                        if (let == 's') state = 'G';
+                        else if (let == ' ') state = 'H';
+                        else state = 'Z';
+                        break;
+                    case 'G':
+                        if(let == ' ')
+                        {
+                            state = 'L';
+                            return true;
+                        }
+                        else state = 'Z';
+                        break;
+                    case 'H':
+                        if (let == ' ') state = 'H';
+                        else if (let == 'Y' || let == 'y') state = 'I';
+                        else state = 'Z';
+                        break;
+                    case 'I':
+                        if (let == 'o') state = 'J';
+                        else state = 'Z';
+                        break;
+                    case 'J':
+                        if (let == 'u') state = 'K';
+                        else state = 'Z';
+                        break;
+                    case 'K':
+                        if (let == ' ')
+                        {
+                            state = 'L';
+                            return true;
+                        }
+                        else state = 'Z';
+                        break;
+                    case 'L': //technically unreachable, but here for completeness anyway
+                        return true;
+                    case 'Z':
+                        if (let == ' ') state = 'A';
                         break;
                     default:
                         throw new ArgumentException($"Unknown State :{state}, Input was {input}");
                 }
+                if (state == 'Z' && let == ' ') state = 'A';
             }
-            throw new NotImplementedException();
+            return (state == 'G' || state == 'K'); //EoS Checker
         }
 
         private static bool CheckJoey(string input)
         {
             char state = 'A';
-            foreach (char c in input)
+            foreach (char let in input)
             {
                 switch(state)
                 {
@@ -129,14 +301,15 @@ namespace NonDeterministicStateMachines
                     default:
                         throw new ArgumentException($"Unknown State :{state}, Input was {input}");
                 }
+                if (state == 'Z' && let == ' ') state = 'A';
             }
-            throw new NotImplementedException();
+            return false;
         }
 
         private static bool CheckHighClass(string input)
         {
             char state = 'A';
-            foreach (char c in input)
+            foreach (char let in input)
             {
                 switch(state)
                 {
@@ -145,14 +318,15 @@ namespace NonDeterministicStateMachines
                     default:
                         throw new ArgumentException($"Unknown State :{state}, Input was {input}");
                 }
+                if (state == 'Z' && let == ' ') state = 'A';
             }
-            throw new NotImplementedException();
+            return false;
         }
 
         private static bool CheckPirate(string input)
         {
             char state = 'A';
-            foreach (char c in input)
+            foreach (char let in input)
             {
                 switch(state)
                 {
@@ -161,8 +335,9 @@ namespace NonDeterministicStateMachines
                     default:
                         throw new ArgumentException($"Unknown State :{state}, Input was {input}");
                 }
+                if (state == 'Z' && let == ' ') state = 'A';
             }
-            throw new NotImplementedException();
+            return false;
         }
 
         private static void Respond(string[] Responses)
